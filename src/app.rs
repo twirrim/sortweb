7e5b529931d37044317a503b5151b969d555fd3f
Twirrim <paulgraydon@hotmail.com>
2024-05-25 10:52:53 -0700

    Initial Bubble Sort visualisation

--- src/app.rs
+++ src/app.rs
@@ -1,25 +1,103 @@
-/// We derive Deserialize/Serialize so we can persist app state on shutdown.
-#[derive(serde::Deserialize, serde::Serialize)]
-#[serde(default)] // if we add new fields, give them default values when deserializing old state
-pub struct TemplateApp {
-    // Example stuff:
-    label: String,
+use ecolor::Color32;
+use egui::*;
+use egui_plot::{Bar, BarChart, Plot};
+use rand::seq::SliceRandom;
+use rand::thread_rng;
 
-    #[serde(skip)] // This how you opt-out of serialization of a field
-    value: f32,
+pub struct BubbleSort {
+    data: Vec<Bar>,
+    current_loc: usize,
+    changed: bool,
+    finished: bool,
 }
 
-impl Default for TemplateApp {
+impl BubbleSort {
+    pub fn new(data: Vec<Bar>) -> Self {
+        let mut sort = BubbleSort {
+            data: data,
+            current_loc: 0,
+            changed: false,
+            finished: false,
+        };
+        sort.data[0].fill = Color32::GREEN;
+        sort
+    }
+
+    pub fn step(&mut self) {
+        // End conditions
+        if self.finished {
+            return;
+        };
+        if self.current_loc + 1 == self.data.len() {
+            if self.changed == false {
+                // Nothing more to do
+                self.finished = true;
+                return;
+            } else {
+                // More work to do, back to the beginning!
+                self.data[self.current_loc].fill = Color32::RED;
+                self.current_loc = 0;
+                self.changed = false;
+            }
+        }
+        if self.data[self.current_loc].value > self.data[self.current_loc + 1].value {
+            // Swap them around
+            let temp = self.data[self.current_loc].value;
+            self.data[self.current_loc].value = self.data[self.current_loc + 1].value;
+            self.data[self.current_loc + 1].value = temp;
+            // Note that we've had to make a change
+            self.changed = true;
+        }
+        // Update location
+        self.data[self.current_loc].fill = Color32::RED;
+        self.current_loc += 1;
+        self.data[self.current_loc].fill = Color32::GREEN;
+    }
+
+    // Make this a trait!
+    pub fn plot_chart(&self, ui: &mut Ui) -> Response {
+        let chart = BarChart::new(self.data.clone()).name("Current State");
+        Plot::new("Normal Distribution Demo")
+            .clamp_grid(true)
+            .y_axis_width(3)
+            .show(ui, |plot_ui| plot_ui.bar_chart(chart))
+            .response
+    }
+}
+
+// TODO: Learn how to disable the state saving.  Don't want it.
+#[derive(serde::Deserialize, serde::Serialize)]
+#[serde(default)] // if we add new fields, give them default values when deserializing old state
+pub struct SortApp {
+    #[serde(skip)]
+    bubble_sort: BubbleSort,
+}
+
+fn make_bar_vec(size: usize) -> Vec<Bar> {
+    // Produce a randomly shuffled vector of numbers first
+    let mut rng = thread_rng();
+    let mut numbers: Vec<usize> = (0..size).collect();
+    numbers.shuffle(&mut rng);
+    println!("Starting State: {:?}", numbers);
+    // Now turn it in to a vector of BarChart Bars
+    let mut bars: Vec<Bar> = vec![];
+    for index in 0..size {
+        let mut bar = Bar::new(index as f64, numbers[index] as f64);
+        bar.fill = Color32::RED;
+        bars.push(bar);
+    }
+    bars
+}
+
+impl Default for SortApp {
     fn default() -> Self {
         Self {
-            // Example stuff:
-            label: "Hello World!".to_owned(),
-            value: 2.7,
+            bubble_sort: BubbleSort::new(make_bar_vec(100)),
         }
     }
 }
 
-impl TemplateApp {
+impl SortApp {
     /// Called once before the first frame.
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
         // This is also where you can customize the look and feel of egui using
@@ -35,7 +113,7 @@ impl TemplateApp {
     }
 }
 
-impl eframe::App for TemplateApp {
+impl eframe::App for SortApp {
     /// Called by the frame work to save state before shutdown.
     fn save(&mut self, storage: &mut dyn eframe::Storage) {
         eframe::set_value(storage, eframe::APP_KEY, self);
@@ -60,36 +138,31 @@ impl eframe::App for TemplateApp {
                     });
                     ui.add_space(16.0);
                 }
+                ui.add(egui::github_link_file!(
+                    "https://github.com/twirrim/sortweb",
+                    "Source code."
+                ));
 
                 egui::widgets::global_dark_light_mode_buttons(ui);
+                ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
+                    powered_by_egui_and_eframe(ui);
+                    egui::warn_if_debug_build(ui);
+                });
             });
         });
 
         egui::CentralPanel::default().show(ctx, |ui| {
             // The central panel the region left after adding TopPanel's and SidePanel's
-            ui.heading("eframe template");
-
-            ui.horizontal(|ui| {
-                ui.label("Write something: ");
-                ui.text_edit_singleline(&mut self.label);
-            });
-
-            ui.add(egui::Slider::new(&mut self.value, 0.0..=10.0).text("value"));
-            if ui.button("Increment").clicked() {
-                self.value += 1.0;
-            }
+            ui.heading("Sort Visualiser");
 
             ui.separator();
+            // Step the bubble sort
+            self.bubble_sort.step();
 
-            ui.add(egui::github_link_file!(
-                "https://github.com/emilk/eframe_template/blob/main/",
-                "Source code."
-            ));
+            self.bubble_sort.plot_chart(ui);
 
-            ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
-                powered_by_egui_and_eframe(ui);
-                egui::warn_if_debug_build(ui);
-            });
+            // And update!
+            ui.ctx().request_repaint();
         });
     }
 }
